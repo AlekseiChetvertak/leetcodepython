09ea597bfbc046b686213cfa15fbaf5d2f7d2a00
Aleksei Chetvertak <150613832+AlekseiChetvertak@users.noreply.github.com>
2023-11-26 16:41:06 +0300

    upd

--- task1Two_Sum/task1twosum.py
+++ task1Two_Sum/task1twosum.py
@@ -1,11 +1,12 @@
-nums = [2,7,11,15]
-m=[0,0]
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
+                if (nums[i] + nums[j] == target):
+                    return[i,j]
+        return[]
+        
 
-target = int(input('enter target'))
-for i in range(0, len(nums)):
-    for j in range(i+1, len(nums)):
-        if (nums[i] + nums[j] == target):
-            return[i,j]
-return[]
+
 
 
